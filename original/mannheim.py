9dcd7c8b1661bac9791f3799868ec55a374b88f4
Holger Bruch <hb@mfdz.de>
2023-04-24 18:01:17 +0200

    Mannheim: update scraper and geojson

--- original/mannheim.py
+++ original/mannheim.py
@@ -20,6 +20,14 @@ class Mannheim(ScraperBase):
         attribution_url="https://www.parken-mannheim.de/impressum",
     )
 
+    """
+    Maps current parking lot names to their former name to provide
+    backward compatibility.
+    """
+    LOT_LEGACY_NAME_MAPPINGS = {
+        "Hauptbahnhof P3, Parkhaus": "Hauptbahnhof P3/P4 Parkhaus",
+    }
+
     def get_lot_data(self) -> List[LotData]:
         timestamp = self.now()
         soup = self.request_soup(self.POOL.public_url)
@@ -27,6 +35,7 @@ class Mannheim(ScraperBase):
         lots = []
 
         # suche: <div id="parkhausliste-ct">
+        # Note: page contains two elements with id parkhausliste-ct, the last one contains the lot data
         div_level1 = soup.find_all('div', id='parkhausliste-ct')[-1]
         # <p style="color: #7a7a7b; padding: 18px 0 8px 0">zuletzt aktualisiert am 19.06.2019, 15:27 Uhr</p>
         date_time = div_level1.find('p')
@@ -39,6 +48,11 @@ class Mannheim(ScraperBase):
         while count < len(div_level3) - 2:
             parking_name = div_level3[count+1].text.strip()
 
+            if parking_name in ('P20', 'UEG (Funktionskarten)'):
+                # workaround: ignore entries which seem to be no parking lots
+                count += 3
+                continue
+            
             parking_free = None
             parking_state = LotData.Status.open
             try:
@@ -51,7 +65,7 @@ class Mannheim(ScraperBase):
             lots.append(
                 LotData(
                     timestamp=timestamp,
-                    id=name_to_legacy_id("mannheim", parking_name),
+                    id=self.name_to_legacy_id(parking_name),
                     lot_timestamp=last_updated,
                     status=parking_state,
                     num_free=parking_free,
@@ -62,7 +76,7 @@ class Mannheim(ScraperBase):
 
     def get_lot_infos(self) -> List[LotInfo]:
         lot_map = {
-            lot.name: lot
+            lot.id: lot
             for lot in self.get_v1_lot_infos_from_geojson("Mannheim")
         }
 
@@ -78,14 +92,30 @@ class Mannheim(ScraperBase):
         count = 0
         while count < len(div_level3) - 2:
             parking_name = div_level3[count+1].text.strip()
+
+            if parking_name in ('P20', 'UEG (Funktionskarten)'):
+                # workaround: ignore entries which seem to be no parking lots
+                count += 3
+                continue
+            
+            lot_id = self.name_to_legacy_id(parking_name)
+
             link = div_level3[count+1].find("a")
             count += 3
             
-            kwargs = vars(lot_map[parking_name])
+            kwargs = vars(lot_map[lot_id]) if lot_id in lot_map else {}
             kwargs.update(dict(
+                id=lot_id,
+                name=parking_name,
+                has_live_capacity=True,
                 public_url=urllib.parse.urljoin(self.POOL.public_url, link["href"])
             ))
 
             lots.append(LotInfo(**kwargs))
 
         return lots
+
+    def name_to_legacy_id(self, lot_name) -> str:
+        legacy_name = self.LOT_LEGACY_NAME_MAPPINGS.get(lot_name, lot_name)
+        return name_to_legacy_id(self.POOL.id, legacy_name)
+

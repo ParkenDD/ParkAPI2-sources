480fb95c955a83337766e991b15988f009429914
Holger Bruch <hb@mfdz.de>
2023-10-10 23:57:25 +0200

    Add hard-coded data fixes

--- original/heidelberg.py
+++ original/heidelberg.py
@@ -5,6 +5,7 @@ from typing import List
 
 from util import *
 
+import warnings
 
 class Heidelberg(ScraperBase):
 
@@ -25,6 +26,13 @@ class Heidelberg(ScraperBase):
         "Referer": "https://parken.heidelberg.de/",
     }
 
+    def _should_ignore(self, parking_lot):
+        if parking_lot['uid'] == '26':
+            warnings.warn(f'Ignoring heidelbergp26kirchheim, as it has non static information')
+            return True
+        
+        return False
+
     def get_lot_data(self) -> List[LotData]:
         timestamp = self.now()
         dataJSON = self.request_json(self.POOL.source_url)
@@ -36,7 +44,12 @@ class Heidelberg(ScraperBase):
         for parking_lot in dataJSON['data']['parkinglocations'] :
             # please keep the name in the geojson-file in the same form as delivered here (including spaces)
             parking_name = ('P'+str(parking_lot['uid'])+' '+parking_lot['name']).strip()
+            parking_id = name_to_legacy_id(self.POOL.id, parking_name)
 
+            # Note: 
+            if self._should_ignore(parking_lot):
+                continue
+            
             try:
                 parking_capacity = int(parking_lot['parkingupdate']['total'])
                 parking_occupied = int(parking_lot['parkingupdate']['current'])
@@ -57,7 +70,7 @@ class Heidelberg(ScraperBase):
                 LotData(
                     timestamp=timestamp,
                     lot_timestamp=last_updated,
-                    id=name_to_legacy_id("heidelberg", parking_name),
+                    id=parking_id,
                     status=parking_state,
                     num_occupied=parking_occupied,
                     capacity=parking_capacity,
@@ -88,17 +101,30 @@ class Heidelberg(ScraperBase):
 
             original_lot = vars(lot_map.get(lot_id)) if lot_id in lot_map else {}
 
+            parking_id = name_to_legacy_id(self.POOL.id, parking_name)
+
+            lat = original_lot.get("latitude")
+            lon = original_lot.get("longitude")
+            if lat is None or lon is None:
+                # Note: the original ParkAPIv1 json had no coords, we explicitly set them here for no
+                if parking_id == 'heidelbergp19campbellbarracks':
+                    lat = 49.38794
+                    lon = 8.68104
+                else:
+                    warnings.warn(f"Lot '{parking_id}' has no coords. Skipping")
+                    continue
+
             lots.append(
                 LotInfo(
-                    id=name_to_legacy_id(self.POOL.id, parking_name),
+                    id=parking_id,
                     type=original_lot.get("type") or 'garage',
                     name=parking_name,
                     capacity=parking_capacity,
                     public_url=parking_lot.get("website"),
                     has_live_capacity=True,
                     address=parking_lot["address"].replace(" ,", ",").strip(" ,").replace(", ", "\n"),
-                    latitude=original_lot.get("latitude"),
-                    longitude=original_lot.get("longitude"),
+                    latitude=lat,
+                    longitude=lon,
                 )
             )
 

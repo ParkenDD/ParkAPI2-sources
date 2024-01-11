c66d30462692c075a9e4014ee5d152d2069ba4e4
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2024-01-11 20:27:28 +0100

    set year to 2024

--- v3/pbw/__init__.py
+++ v3/pbw/__init__.py
@@ -1,5 +1,5 @@
 """
-Copyright 2023 binary butterfly GmbH
+Copyright 2024 binary butterfly GmbH
 Use of this source code is governed by an MIT-style license that can be found in the LICENSE.txt.
 """
 

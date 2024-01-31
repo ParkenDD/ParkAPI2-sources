2e3f86d8c6e1f3f7e63c7cf9f30875ecd79345cc
Ernesto Ruge <ernesto.ruge@binary-butterfly.de>
2024-01-31 07:35:00 +0100

    normalized xlsx converter as intermediate class (#66)
    * normalized xlsx converter as intermediate class
    
    * linting

--- common/base_converter/__init__.py
+++ common/base_converter/__init__.py
@@ -6,6 +6,7 @@ Use of this source code is governed by an MIT-style license that can be found in
 from .base_converter import BaseConverter
 from .csv_converter import CsvConverter
 from .json_converter import JsonConverter
+from .normalized_xlsx_converter import NormalizedXlsxConverter
 from .pull_converter import PullConverter
 from .xlsx_converter import XlsxConverter
 from .xml_converter import XmlConverter
